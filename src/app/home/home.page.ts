e3617abd67e9907321c047802ec77dcf4770b8fc
StenioHenrique <steniohmsilva@gmail.com>
2021-09-26 21:28:48 -0300

    Adicionando classificação de IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,8 +18,21 @@ export class HomePage {
   }
 
   onCalculate() {
-    const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    const imc = this.weight / (this.height * this.height); 
+    var classificacao; 
+    if (imc < 18.5) { 
+      classificacao = "Magreza"; 
+    } else if (imc >= 18.5 && imc <= 24.9) { 
+      classificacao = "Normal"; 
+    } else if (imc >= 25 && imc <= 29.9) { 
+      classificacao = "Sobrepeso";
+    } else if (imc >= 30 && imc <= 39.9) { 
+      classificacao = "Obesidade"; 
+    } else { 
+      classificacao = "Obesidade grave"; 
+    }
+  
+    this.showMessage(`IMC = ${imc.toFixed(2)} - ${classificacao}`);
   }
 
   async showMessage(msg: string) {
